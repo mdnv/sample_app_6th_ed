b74d0ef55b8d26d021bcebd84bcbdb330ade51bd
mdnv <mdnvng@gmail.com>
2020-05-19 10:18:25 +0700

    new release

--- app/controllers/api/static_pages_controller.rb
+++ app/controllers/api/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class Api::StaticPagesController < Api::ApiController
   def home
     if logged_in?
-      @feed_items = current_user.feed.page(params[:page]).per(params[:per])
+      @feed_items = current_user.feed.page(params[:page])
     else
       head :ok
     end
